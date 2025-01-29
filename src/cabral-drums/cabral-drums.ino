322c8634c0225178218c8572088817de562581d2
Arthur Cabral <acl_360@yahoo.com.br>
2025-01-29 13:40:10 -0300

    Ajuste na lógica de detecção de batidas: melhoria no buffer de saída e simplificação do código de debounce

--- src/cabral-drums/cabral-drums.ino
+++ src/cabral-drums/cabral-drums.ino
@@ -1,14 +1,14 @@
-#define NUM_PADS 12  
-const int piezoPins[NUM_PADS] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
+#define NUM_PADS 12
+const int piezoPins[NUM_PADS] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 };
 
-const int padThresholds[NUM_PADS] = {10, 1023, 1023, 1023, 1023, 1023, 
-                                    1023, 1023, 1023, 1023, 1023, 1023};
+const int padThresholds[NUM_PADS] = { 12, 1023, 1023, 1023, 1023, 1023,
+                                      1023, 1023, 1023, 1023, 1023, 1023 };
 
-unsigned long lastHitTime[NUM_PADS] = {0};
-const int debounceTime = 50;  // Reduzi o debounce para 50ms
-const int peakDetectionWindow = 20;  // Janela de detecção mais curta para resposta rápida
-int peakValue[NUM_PADS] = {0};
-bool hitDetected[NUM_PADS] = {false};
+unsigned long lastHitTime[NUM_PADS] = { 0 };
+const int debounceTime = 50;
+const int peakDetectionWindow = 20;
+int peakValue[NUM_PADS] = { 0 };
+bool hitDetected[NUM_PADS] = { false };
 
 void setup() {
   Serial.begin(115200);
@@ -18,22 +18,32 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  char buffer[10];
+
   for (int i = 0; i < NUM_PADS; i++) {
     int piezoValue = analogRead(piezoPins[i]);
 
-    // Se detectar uma batida acima do threshold e não estiver em debounce
     if (piezoValue > padThresholds[i]) {
-      if (!hitDetected[i] && millis() - lastHitTime[i] > debounceTime) {
-        peakValue[i] = piezoValue;  
-        hitDetected[i] = true;  // Marca que uma batida foi detectada
-        lastHitTime[i] = millis();
-        Serial.println(peakValue[i]);
+      if (!hitDetected[i] && (currentTime - lastHitTime[i] > debounceTime)) {
+        peakValue[i] = piezoValue;
+        hitDetected[i] = true;
+        lastHitTime[i] = currentTime;
+
+        // play pad
+        itoa(i, buffer, 10);
+        int len = strlen(buffer);
+        buffer[len] = '-';
+        itoa(peakValue[i], buffer + len + 1, 10);
+        buffer[len + strlen(buffer + len + 1)] = '\n';
+        Serial.write(buffer, strlen(buffer));
+
+
       }
     } else {
-      // Reset da detecção após a janela de tempo
-      if (hitDetected[i] && millis() - lastHitTime[i] > peakDetectionWindow) {
+      if (hitDetected[i] && (currentTime - lastHitTime[i] > peakDetectionWindow)) {
         hitDetected[i] = false;
-        peakValue[i] = 0;  // Reseta o valor do pico para a próxima batida
+        peakValue[i] = 0;
       }
     }
   }
